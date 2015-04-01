24acc853aaec4f2520b362506a01900bd9f3d0f3
natew <natewienert@gmail.com>
2015-04-01 11:14:18 -0700

    cleaner syntax

--- src/lib/Page.js
+++ src/lib/Page.js
@@ -4,18 +4,16 @@ import ShouldUpdate from './ShouldUpdate';
 import RoutedViewListMixin from 'reapp-routes/react-router/RoutedViewListMixin';
 import AutoBind from './AutoBind';
 
-const Base = React.createClass(
-  Object.assign({},
-    ShouldUpdate,
-    {
-      contextTypes: ContextTypes,
-      mixins: [RoutedViewListMixin],
-      render: function() {}
-    }
-  )
-);
-
-Object.assign(Base.prototype, AutoBind);
+const Base = React.createClass(Object.assign(
+  {},
+  AutoBind,
+  ShouldUpdate,
+  {
+    contextTypes: ContextTypes,
+    mixins: [RoutedViewListMixin],
+    render: function() {}
+  }
+));
 
 class Page extends Base {
   constructor(props, shouldAutoBind = true) {
