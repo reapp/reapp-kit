2a0303fd5c19aa6de0489858a187758f6bed6bb4
natew <natewienert@gmail.com>
2015-04-01 10:37:56 -0700

    autobinding

--- src/lib/Component.js
+++ src/lib/Component.js
@@ -2,8 +2,26 @@ var React = require('react');
 var ContextTypes = require('./ContextTypes');
 var ShouldUpdate = require('./ShouldUpdate');
 
-var Component = React.Component;
-Component.contextTypes = ContextTypes;
-Component.shouldComponentUpdate = ShouldUpdate.shouldComponentUpdate;
+var Base = React.Component;
+Base.contextTypes = ContextTypes;
+Base.shouldComponentUpdate = ShouldUpdate.shouldComponentUpdate;
+
+class Component extends Base {
+  constructor(props, shouldAutoBind = true) {
+    super(props);
+    if (shouldAutoBind)
+      this.autoBind();
+  }
+
+  bind(methods) {
+    methods.forEach(method => this[method] = this[method].bind(this));
+  }
+
+  autoBind() {
+    const classProps = Object.getOwnPropertyNames(this.constructor.prototype);
+    const classFuncs = classProps.filter(prop => typeof this[prop] === 'function');
+    this.bind(classFuncs);
+  }
+}
 
 module.exports = Component;
