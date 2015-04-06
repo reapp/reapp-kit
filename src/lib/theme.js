3f9a72a93b846d33b9b67e7f682d52a6af38dce4
natew <natewienert@gmail.com>
2015-04-06 12:46:49 -0700

    add theme converter for convenience

--- src/lib/theme.js
+++ src/lib/theme.js
@@ -4,6 +4,8 @@ var theme;
 
 module.exports = function(t) {
   if (t) {
+    t = convertRawTheme(t);
+
     UI.addConstants.apply(UI, t.constants);
     UI.addStyles.apply(UI, t.styles);
     UI.addAnimations.apply(UI, t.animations);
@@ -11,4 +13,18 @@ module.exports = function(t) {
   }
 
   return theme;
+}
+
+// user may not want to configure a whole theme and just pass
+// in the raw theme, lets convert it for them
+function convertRawTheme(theme) {
+  if (!Array.isArray(theme.constants)) {
+    return {
+      constants: Object.keys(theme.constants).map(key => theme.constants[key]),
+      styles: [].concat(theme.styles),
+      animations: [].concat(theme.animations)
+    }
+  }
+  else
+    return theme;
 }
